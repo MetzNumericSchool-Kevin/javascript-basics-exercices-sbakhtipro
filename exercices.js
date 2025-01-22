5ecd0aaca0bbe0a433e3c6cedb494b38a8a35c75
sbakhtipro <sarah.bakhti.pro@gmail.com>
2025-01-22 09:16:53 +0100

    ex 2

--- exercices.js
+++ exercices.js
@@ -7,6 +7,9 @@ let prixPotionSoin = 3
 let quantitePotionSoin = 10
 let ouvertureBoutique = false
 
+let affichageMenu = `Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ?\n1. Le nom de la boutique\n2. Le nom du Sorcier\n3. Le prix d'un potion de soin\n4. La quantité d'une potion de soin`
+let choix = 0
+
 if (ouvertureBoutique) {
     console.log(`Bienvenue dans la boutique ${nomBoutique} Aventurier !`)
 }
@@ -16,20 +19,22 @@ else {
 }
 
 function menu() {
-    let affichageMenu = console.log(`Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ?\n1. Le nom de la boutique\n2. Le nom du Sorcier\n3. Le prix d'un potion de soin\n4. La quantité d'une potion de soin`)
-    let choix = prompt("Que souhaitez vous faire? ")    
-    if (parseInt(choix) === 1) {
-        console.log(nomBoutique)
-    }
-    else if (parseInt(choix) === 2) {
-        console.log(nomSorcier)
-    }
-    else if (parseInt(choix) === 3) {
-        console.log(prixPotionSoin)}
-    else if (parseInt(choix) === 4) {
-        console.log(quantitePotionSoin)
-    }
-    else {
-        console.log(`Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix !`)
+    while (choix === 0) {
+        console.log(affichageMenu)    
+        if (parseInt(choix) === 1) {
+            console.log(nomBoutique)
+        }
+        else if (parseInt(choix) === 2) {
+            console.log(nomSorcier)
+        }
+        else if (parseInt(choix) === 3) {
+            console.log(prixPotionSoin)}
+        else if (parseInt(choix) === 4) {
+            console.log(quantitePotionSoin)
+        }
+        else {
+            console.log(`Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix !`)
+            choix = 0
+        }
     }
 }
