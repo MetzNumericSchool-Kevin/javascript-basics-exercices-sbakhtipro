eb882663c9eea43d583c5e362af0af5a6fc4b7a5
sbakhtipro <sarah.bakhti.pro@gmail.com>
2025-01-22 11:57:06 +0100

    ex 6

--- exercices.js
+++ exercices.js
@@ -1,9 +1,12 @@
 const nomBoutique = "exemple";
 let nomSorcier = "Archibald";
-let nbPotions = 5;
 let prixPotionSoin = 3;
 let ouvertureBoutique = false;
-let stockPotionsSoin = 50
+let stockBoutique = {
+    "potions_soin":50,
+    "potions_2":0,
+    "potions_3":0
+}
 
 let affichageMenu = `Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ?\n1. Le nom de la boutique\n2. Le nom du Sorcier\n3. Le prix d'un potion de soin\n4. La quantité d'une potion de soin`
 let choix = 0
@@ -28,7 +31,7 @@ function menu() {
             choix = prixPotionSoin
             break;
         case 4 :
-            choix = stockPotionsSoin
+            choix = stockBoutique.potions_soin
             break;
         default :
             console.log(`Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix !`);
@@ -40,30 +43,31 @@ let choixMenu = menu()
 
 console.log(choixMenu)
 
-quantitePotionSoin = prompt("Quantité potion soin : ")
+quantitePotionSoin = parseInt(prompt("Quantité potion soin : "))
 let coutTotal = quantitePotionSoin * prixPotionSoin
 console.log(`Le prix pour ${quantitePotionSoin} potion soin est ${coutTotal}`)
 
 let argentAventurier = 100;
-let stockAventurier = [
-    {"potions_soin":0}
-];
+let stockAventurier = {
+    "potions_soin":0,
+    "potions_2":0,
+    "potions_3":0
+};
 
-if (argentAventurier >= coutTotal && stockPotionsSoin >= quantitePotionSoin) {
+if (argentAventurier >= coutTotal && stockBoutique.potions_soin >= quantitePotionSoin) {
     argentAventurier-=coutTotal
-    let potionSoinStock = stockAventurier[0].potions_soin
-    potionSoinStock+=quantitePotionSoin
-    stockPotionsSoin-=quantitePotionSoin
+    stockBoutique.potions_soin-=quantitePotionSoin
+    stockAventurier.potions_soin+=quantitePotionSoin
 }
 else {
     console.log("Pas assez d'argent")
 }
 
 console.log(stockAventurier)
-console.log(stockPotionsSoin)
+console.log(stockBoutique)
 
 choixMenu = menu()
 
-
-
+// - Créé un tableau qui contient les noms de trois potions que tu vends dans ta boutique.
+// - Affiche le tableau directement dans la console.
 
