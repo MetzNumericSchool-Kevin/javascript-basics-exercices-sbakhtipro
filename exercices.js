a7b92aa6d5eda26642c9a5d76f779d3ef2dc2d0c
sbakhtipro <sarah.bakhti.pro@gmail.com>
2025-01-22 11:40:19 +0100

    ex 5

--- exercices.js
+++ exercices.js
@@ -3,6 +3,7 @@ let nomSorcier = "Archibald";
 let nbPotions = 5;
 let prixPotionSoin = 3;
 let ouvertureBoutique = false;
+let stockPotionsSoin = 50
 
 let affichageMenu = `Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ?\n1. Le nom de la boutique\n2. Le nom du Sorcier\n3. Le prix d'un potion de soin\n4. La quantité d'une potion de soin`
 let choix = 0
@@ -10,7 +11,6 @@ let choix = 0
 if (ouvertureBoutique) {
     console.log(`Bienvenue dans la boutique ${nomBoutique} Aventurier !`);
 }
-
 else {
     console.log(`La boutique ${nomBoutique} est fermée, revenez plus tard Aventurier !`);
 }
@@ -28,7 +28,7 @@ function menu() {
             choix = prixPotionSoin
             break;
         case 4 :
-            choix = quantitePotionSoin
+            choix = stockPotionsSoin
             break;
         default :
             console.log(`Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix !`);
@@ -40,9 +40,29 @@ let choixMenu = menu()
 
 console.log(choixMenu)
 
-let quantitePotionSoin = prompt("Quantité potion soin : ")
-console.log(`Le prix pour ${quantitePotionSoin} potion soin est ${quantitePotionSoin * prixPotionSoin}`)
+quantitePotionSoin = prompt("Quantité potion soin : ")
+let coutTotal = quantitePotionSoin * prixPotionSoin
+console.log(`Le prix pour ${quantitePotionSoin} potion soin est ${coutTotal}`)
 
+let argentAventurier = 100;
+let stockAventurier = [
+    {"potions_soin":0}
+];
+
+if (argentAventurier >= coutTotal && stockPotionsSoin >= quantitePotionSoin) {
+    argentAventurier-=coutTotal
+    let potionSoinStock = stockAventurier[0].potions_soin
+    potionSoinStock+=quantitePotionSoin
+    stockPotionsSoin-=quantitePotionSoin
+}
+else {
+    console.log("Pas assez d'argent")
+}
+
+console.log(stockAventurier)
+console.log(stockPotionsSoin)
+
+choixMenu = menu()
 
 
 
